e0222058baefb8328adf651a11bdf29ed3621c73
Nico <nicolas.callaghan@gmail.com>
2025-06-29 23:53:32 +0200

    first commit

---
+++ login.js
@@ -0,0 +1,37 @@
+document.querySelector('#register').addEventListener('click', function(){
+    const data = {
+        email: document.querySelector('#registerName').value, 
+        email: document.querySelector('#registerEmail').value, 
+        password: document.querySelector('#registerPassword').value
+    }
+
+    fetch('http://localhost:3000/users/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    }).then(response => response.json())
+    .then(data => 
+        {if(data.result === true) {
+            window.location.assign('index.html')
+        }}
+    )
+})
+
+document.querySelector('#connection').addEventListener('click', function(){
+    const data = {
+        email: document.querySelector('#connectionEmail').value, 
+        password: document.querySelector('#connectionPassword').value
+    }
+
+    fetch('http://localhost:3000/users/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    }).then(response => response.json())
+    .then(data => 
+        {if(data.result === true) {
+            window.location.assign('index.html')
+        }}
+    )
+})
+
